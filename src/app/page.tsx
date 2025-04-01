699b4d8b487d52f3634c43bf0eb144bd2b5c4d64
pragmaticAweds <jimohafeez738@gmail.com>
2025-04-01 12:13:33 +0100

    chore: setting up layout

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+//  <Image
+//    className="dark:invert"
+//    src="/next.svg"
+//    alt="Next.js logo"
+//    width={180}
+//    height={38}
+//    priority
+//  />;
+
+export default function Home() {
+  return <main>Welcome To Fundable!!!</main>;
+}
